adc0159a09d0fea413fbf97ee1d655450d5419a7
Anwesh43 <anweshthecool0@gmail.com>
2019-02-06 10:16:38 +0530

    adding libraries and adding constants

--- circleshifterrotlineview/src/main/java/com/anwesh/uiprojects/circleshifterrotlineview/CircleShifterRotLineView.kt
+++ circleshifterrotlineview/src/main/java/com/anwesh/uiprojects/circleshifterrotlineview/CircleShifterRotLineView.kt
@@ -4,3 +4,20 @@ package com.anwesh.uiprojects.circleshifterrotlineview
  * Created by anweshmishra on 06/02/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val circles : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.8f
+val foreColor : Int = Color.parseColor("#311B92")
+val backColor : Int = Color.parseColor("#BDBDBD")
+
