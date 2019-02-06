c1ab36fa423e62c04ba03da57facdbab76de7e05
Anwesh43 <anweshthecool0@gmail.com>
2019-02-06 10:45:11 +0530

    adding CircleShifterRotLineView class

--- circleshifterrotlineview/src/main/java/com/anwesh/uiprojects/circleshifterrotlineview/CircleShifterRotLineView.kt
+++ circleshifterrotlineview/src/main/java/com/anwesh/uiprojects/circleshifterrotlineview/CircleShifterRotLineView.kt
@@ -68,3 +68,21 @@ fun Canvas.drawCSRLNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class CircleShifterRotLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
